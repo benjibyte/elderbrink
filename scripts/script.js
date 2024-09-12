3b2c697049aef0a84e94a83585db79089c313707
scoolywooly <benjipixel@proton.me>
2024-09-12 16:45:01 -0500

    added report button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -32,38 +32,69 @@ function loadHomeContent(event) {
 // When the document loads in, I want to keep the record
 // buttons hidden until the user clicks on the Records link
 function displayReportTiles() {
-    let tiles = document.getElementsByClassName("tiles");
-    if (tiles.style.display === "none") {
-        tiles.style.display = "";
-    }
+
+    // getting an ellement by it's class name selects a group rather than a specific ellement so if i want to change it's style
+    // I have to use a loop, reccomeneded a for loop.
+    document.getElementById("scroll-tiles").style.display = "grid";
+
+}
+
+function hideReportTiles() {
+    DocumentFragment.getElementById("scroll-tiles").style.display = "none";
 }
 
 // Allow the User to switch webpages "virtually" by
-// loading in the home content when an "a" tag is clicked
+// loading in the home content when an "button" tag is clicked
 function loadReportsContent(event) {
     event.preventDefault();
 
+    console.log("Loading Report Content...")
 
     // Grab Article headline from txt file on server
     fetch("texts/reports-headline.txt")
         .then(response => response.text())
         .then(data => {
             document.getElementById("page-headline").innerText = data;
-        })
+            document.getElementById("page-title").innerText = "📁";
 
+            /* for the Reports section of the website,
+             I can't just change the title to data of it's fiel since there are many dates
+             per reports file. so I for this one, I'm just going to set the 
+             title to something blank.
+            */ 
+        });
+    displayReportTiles();
 
-    // Grab Article text from server
-    fetch("texts/reports-article.txt")
-        .then(response => response.text())
-        .then(data => {
-            // grabs the paragraph tag with the "article text" id and slides the data from the txt file into that slot.
-            document.getElementById("page-text").innerHTML = data;
-
-        })
-        .catch(error => console.error("Error fetching the text file:", error))
+    
 
 };
 
+// Get the specific record.txt file and display it
+function loadSpecificReport(fileName) {
+
+    fetch(fileName)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network Response is suffering: " + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+
+            // It works!
+
+            document.getElementById("page-text").innerHTML = data;
+            
+        })
+        .catch(error => {
+
+            // It doesn't work :(
+            console.error("There is an error with the fetch");
+            document.getElementById("page-text").innerText = "Error 404?... maybe.";
+        })
+
+  
+}
 
 
 
@@ -103,5 +134,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Call the function to load the Schedule content. (adds the onclick to the html without having to actually edit the html)
     document.getElementById("load-schedule").addEventListener("click", loadScheduleContent);
     // Call the function to load the Report content. (adds the onclick to the html without having to actually edit the html)
+
+    document.getElementById("load-reports").addEventListener("click", displayReportTiles);
     document.getElementById("load-reports").addEventListener("click", loadReportsContent);
-})
+})          
